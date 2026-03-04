c376d44a2816ce383953a9ad96f5579605189876
Michael Allen <michaelallen@pdx1-shared-a4-04.dreamhost.com>
2026-03-04 10:22:23 -0800

    added mod_socialbades to class

--- tmpl/default.php
+++ tmpl/default.php
@@ -3,7 +3,7 @@
 defined('_JEXEC') or die; 
 ?>
 <?php if(count($badges)>0) : ?>
-<ul class="nav nav-pills nav-justified menu<?php echo $moduleclass_sfx;?>">
+<ul class="mod-socialbadges nav nav-pills nav-justified menu<?php echo $moduleclass_sfx;?>">
 
 	<?php	foreach ($badges as $type=>$badge) : ?>
 		<li><a href="<?php echo $badge["url"]; ?>"><span class="badge-<?php echo $type; ?> badge-4x block"> </span><span class="badge-title"><?php echo $badge["title"]; ?></span></a></li>
