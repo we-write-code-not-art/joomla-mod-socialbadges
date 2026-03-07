eb8075666e3470ccc1bd62fec33eda9a5b23d3c8
Michael Allen <michaelallen@pdx1-shared-a4-04.dreamhost.com>
2026-03-07 12:29:49 -0800

    Release 6.0.5

--- tmpl/collapsed.php
+++ tmpl/collapsed.php
@@ -6,7 +6,7 @@ defined('_JEXEC') or die;
 <ul class="nav navbar-nav menu<?php echo $moduleclass_sfx;?>">
 
 	<?php	foreach ($badges as $type=>$badge) : ?>
-		<li><a href="<?php echo $badge["url"]; ?>"><span class="badge-2x badge-<?php echo $type; ?>"> </span><span class="badge-title"><?php echo $badge["title"]; ?></span></a></li>
+		<li><a href="<?php echo $badge["url"]; ?>" target="_blank" rel="noopener"><span class="badge-2x badge-<?php echo $type; ?>"> </span><span class="badge-title"><?php echo $badge["title"]; ?></span></a></li>
 <?php endforeach ?>
 	</ul>
 	<div class="clearfix"></div>
--- tmpl/default.php
+++ tmpl/default.php
@@ -6,7 +6,7 @@ defined('_JEXEC') or die;
 <ul class="mod-socialbadges nav nav-pills nav-justified menu<?php echo $moduleclass_sfx;?>">
 
 	<?php	foreach ($badges as $type=>$badge) : ?>
-		<li><a href="<?php echo $badge["url"]; ?>"><span class="badge-<?php echo $type; ?> badge-4x block"> </span><span class="badge-title"><?php echo $badge["title"]; ?></span></a></li>
+		<li><a href="<?php echo $badge["url"]; ?>" target="_blank" rel="noopener"><span class="badge-<?php echo $type; ?> badge-4x block"> </span><span class="badge-title"><?php echo $badge["title"]; ?></span></a></li>
 <?php endforeach ?>
 	</ul>
 	<div class="clearfix"></div>
