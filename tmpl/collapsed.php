b5fed618458fb23903f5a89ad6bf3f0889bc17df
Michael Allen <michaelallen@pdx1-shared-a4-04.dreamhost.com>
2026-03-19 19:34:25 -0700

    added mod-socialbadges to collapsed layout ul class

--- tmpl/collapsed.php
+++ tmpl/collapsed.php
@@ -3,10 +3,10 @@
 defined('_JEXEC') or die; 
 ?>
 <?php if(count($badges)>0) : ?>
-<ul class="nav navbar-nav menu<?php echo $moduleclass_sfx;?>">
+<ul class="mod-socialbadges nav navbar-nav menu<?php echo $moduleclass_sfx;?>">
 
-	<?php	foreach ($badges as $type=>$badge) : ?>
-		<li><a href="<?php echo $badge["url"]; ?>" target="_blank" rel="noopener"><span class="badge-2x badge-<?php echo $type; ?>"> </span><span class="badge-title"><?php echo $badge["title"]; ?></span></a></li>
+	<?php foreach ($badges as $type=>$badge) : ?>
+		<li><a href="<?php echo $badge["url"]; ?>" target="_blank" rel="noopener"><span class="badge-2x badge-<?php echo $type; ?>"> </span><span class="badge-title"><?php echo $badge["title"]; ?></span></a></li>
 <?php endforeach ?>
 	</ul>
 	<div class="clearfix"></div>
